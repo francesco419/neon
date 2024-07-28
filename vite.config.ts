c70d7cecedb901f46a8937c97f22619cb68b5ecf
Lee Sang Hyeon <francesco419@naver.com>
2024-07-28 23:47:14 +0900

    0728

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,26 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [
-      { find: '@', replacement: '/src' },
-      { find: '@components', replacement: '/src/components' },
-      { find: '@context', replacement: '/src/context' }
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components')
+      },
+      { find: '@context', replacement: path.resolve(__dirname, 'src/context') }
     ]
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {}
+      },
+      external: []
+    }
   }
 });
