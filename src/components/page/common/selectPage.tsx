1fe6a2db7c2aadff0bd27b320abddb723440bb07
Lee Sang Hyeon <francesco419@naver.com>
2024-08-19 23:40:11 +0900

    plans

--- src/components/page/common/selectPage.tsx
+++ src/components/page/common/selectPage.tsx
@@ -5,10 +5,27 @@ const Container = styled.div`
   width: 100vw;
   height: 100vh;
   padding: 20px 0;
-  background-color: #000;
+  background-color: #f8f9fa;
   box-sizing: border-box;
 `;
+//https://creativebooster.net/blogs/colors/gray-color-palettes
 
+//edefea
+/**
+ * e4e1dc
+ * d1cbc1
+ * ada195
+ * 74695a
+ */
+
+//f8f9fa
+/**
+ * e9ecef
+ * dee2e6
+ * ced4da
+ * a7afb6
+ * darker-down
+ */
 const GridBox = styled.div`
   display: grid;
   grid-template-rows: repeat(3, 1fr);
@@ -17,7 +34,7 @@ const GridBox = styled.div`
   margin: 0 auto;
   padding: 0 20px;
   box-sizing: border-box;
-  background-color: #000;
+  background-color: #f8f9fa;
   gap: 10px;
 `;
 
@@ -76,3 +93,16 @@ box-shadow: 0 0 .2rem #fff,
   justify-content: center;
 `;
            */
+
+/**
+ * 
+ *h1 - head
+ img(half) / opening
+ h2 - sub
+ content
+ h2 - sub
+ content
+ call-to action
+ */
+
+//first page = recent post
